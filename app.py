6b9e260af0edd2d5d5e1df4ac652d9fcc5d29d40
RozyBrt <rozymc99@gmail.com>
2024-02-28 18:19:55 +0700

    new project

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('Belajar steamlit')
+
+# Membuat header
+st.header('Ini adalah header')
+
+# Membuat subHeader
+st.subheader('Ini adalah subheader')
+
+# membuat markdown
+st.markdown('# Ini adalah markdown 1')
+st.markdown('## Ini adalah markdown 2')
+st.markdown('### Ini adalah markdown 3')
+st.markdown('#### Ini adalah markdown 4')
+st.markdown('##### Ini adalah markdown 5')
